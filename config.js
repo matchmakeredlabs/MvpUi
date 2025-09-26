9b7eee7b234415bd3517433f7a6c9be8548bdf49
verybigballoon <bogdanyr4@gmail.com>
2025-09-26 11:25:34 -0700

    endpoint change .. again

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default class config {
-    // static backEndUrl = "https://stage.matchmakeredlabs.net";
-    static backEndUrl = "http://localhost:7071";
+    static backEndUrl = "https://stage.matchmakeredlabs.net";
+    // static backEndUrl = "http://localhost:7071";
     static sessionTag = "mm";
 }
