a8e2c85939b17f7b77e678464c3a8f37e0c6e3fd
schirica7 <stefanchirica2@gmail.com>
2026-08-14 18:29:13 -0400

    Trying to create GitHub action for 'app' url

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export default class config {
-    static backEndUrl = "https://stage.matchmakeredlabs.net";
+    //static backEndUrl = "https://stage.matchmakeredlabs.net";
+    static backEndUrl = "https://app.matchmakeredlabs.net";
     //static backEndUrl = "http://localhost:7071";
     static sessionTag = "mm";
 }
